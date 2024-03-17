b0c584a2fd89fc019e43d9960a6188788902f196
H04x <101000391+LovesVettie@users.noreply.github.com>
2024-03-17 17:09:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import requests
+from colorama import Fore, Back, Style
 
 wordlist = []
 dosya_adi = 'wordlist.txt'
-
+sitegir = input("Lütfen Bir Site Girin (örnek: yemeksepeti.com):  ")
 try:
     with open(dosya_adi, "r") as dosya:
         wordlist.extend(dosya.read().splitlines())
 except FileNotFoundError as e:
-    print(f"Dosya BUlunamadı: {e}")
+    print(f"Dosya Bulunamadı: {e}")
 
 for i in wordlist:
-    print(i) # wordlistin içini okumak içim
+    print(i+sitegir)
