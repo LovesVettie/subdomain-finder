4d1c27b0c0ea14783433246fb1f91bf80bcc427a
LordSUCCESS <145927861+LordSUCCSES@users.noreply.github.com>
2024-03-17 16:58:18 +0300

    wordlist

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 import requests
+
+wordlist = []
+dosya_adi = 'wordlist.txt'
+
+try:
+    with open(dosya_adi, "r") as dosya:
+        wordlist.extend(dosya.read().splitlines())
+except FileNotFoundError as e:
+    print(f"Dosya BUlunamadı: {e}")
+
+for i in wordlist:
+    print(i) # wordlistin içini okumak içim
